8d5569422dc486879809df9106685a669b228942
Quinn Xie <antlossway@gmail.com>
2024-02-07 08:24:34 +0700

    reomve blog, because shutdown aws wordpress instance

--- src/app/data.js
+++ src/app/data.js
@@ -1,23 +1,23 @@
 export const menuItems = [
-    {
-        id: 1,
-        name: 'home',
-        url: '/',
-    },
-    {
-        id: 2,
-        name: 'courses',
-        url: '/courses',
-    },
+  {
+    id: 1,
+    name: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "courses",
+    url: "/courses",
+  },
 
-    {
-        id: 4,
-        name: 'testimonial',
-        url: '/#testimonial'
-    },
-    {
-        id: 5,
-        name: 'blog',
-        url: '/blog',
-    },
+  {
+    id: 4,
+    name: "testimonial",
+    url: "/#testimonial",
+  },
+  // {
+  //     id: 5,
+  //     name: 'blog',
+  //     url: '/blog',
+  // },
 ]
